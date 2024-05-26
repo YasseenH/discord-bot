729ef1164fd88f006060ce1c753591b638001b23
Yasso Hilal <yasseenhilal@utexas.edu>
2024-05-26 02:21:40 -0500

    First Function of Hello

--- bot.py
+++ bot.py
@@ -1,16 +1,21 @@
-import os #SSSSS
+import os
 
-from discord import Intents, Client
+from discord import Client, Intents
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents: Intents = Intents.default()
-client: Client = Client(intents=intents)
+client = commands.Bot(command_prefix='!')
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord Server')
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hello, I am alive!")
 
 client.run(TOKEN)
