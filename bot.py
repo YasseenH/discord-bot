895cc69bcbc2f70bfc23ced8937c0623e3f6f5ef
YasseenH <yasseenhilal@utexas.edu>
2024-05-29 11:44:45 -0500

    Fixed Joke API and Function outputs a random joke

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 #Import required dependicies
-import os
+import os, aiohttp
 from discord import Intents
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -7,31 +7,31 @@ import requests
 import json
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents: Intents = Intents.default()
 intents.members = True
-client = commands.Bot(command_prefix='!', intents=intents)
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord Server')
+    print(f'{bot.user} has connected to Discord Server')
 
-@client.command()
+@bot.command()
 async def hello(ctx):
     await ctx.send("Hello, I am alive!")
 
-@client.command()
+@bot.command()
 async def joke(ctx):
-    joke_url = "https://joke3.p.rapidapi.com/v1/joke"
-    JOKE_KEY = os.getenv('JOKE_API_KEY')
+    url = "https://official-joke-api.appspot.com/random_joke"
 
-    headers = {
-	"X-RapidAPI-Key": JOKE_KEY,
-	"X-RapidAPI-Host": "joke3.p.rapidapi.com"
-}
+    payload = {}
+    headers = {}
 
-    response = requests.get(joke_url, headers=headers)
-    await ctx.send(response.text)
+    response = requests.request("GET", url, headers=headers, data=payload)
 
-client.run(TOKEN)
+    await ctx.send(json.loads(response.text)['setup'])
+    await ctx.send(json.loads(response.text)['punchline'])
+
+
+bot.run(os.getenv('DISCORD_TOKEN'))
